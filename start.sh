cdb79cbe1ab01cedf4c458460a4dd2a42a4764bd
Hope Dorkenoo <compounddork@gmail.com>
2023-09-11 22:11:28 +0000

    deploy file changes

--- start.sh
+++ start.sh
@@ -6,9 +6,6 @@ set -e
 #     touch .env
 # fi
 
-echo "run db migration"
-echo "=====> $DB_SOURCE"
-/app/migrate -path /app/migrations -database "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=disable" -verbose up
 
 
 echo "start up"
